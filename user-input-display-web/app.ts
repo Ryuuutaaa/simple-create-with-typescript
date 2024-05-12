6539167a1f38279fa4601286988477ddd11bbf34
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-13 00:59:57 +0800

    get user interface in user input:

---
+++ user-input-display-web/app.ts
@@ -0,0 +1,3 @@
+const number1 = document.getElementById("num1") as HTMLInputElement;
+const number2 = document.getElementById("printBtn") as HTMLButtonElement;
+const number3 = document.getElementById("enteredNumber") as HTMLSpanElement;
