a10fbbb305145f8420da7efab8c022191f0cf692
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-19 00:57:55 +0800

    add number

---
+++ calculator/app.ts
@@ -0,0 +1,17 @@
+const number1 = document.getElementById("num1") as HTMLInputElement;
+const number2 = document.getElementById("num2") as HTMLInputElement;
+
+const AddBtn = document.getElementById("Add") as HTMLButtonElement;
+const SubBtn = document.getElementById("Sub") as HTMLButtonElement;
+const MulBtn = document.getElementById("Mult") as HTMLButtonElement;
+const DivBtn = document.getElementById("Divide") as HTMLButtonElement;
+
+const printResult = document.getElementById("result") as HTMLOutputElement;
+
+function addNumber() {
+  let a = parseFloat(number1.value);
+  let b = parseFloat(number2.value);
+
+  let result = a + b;
+  printResult.textContent = result.toString();
+}
